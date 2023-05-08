09205fd5d5c3c42aa2c8240554fde0e756811490
ttb06 <trantuanbinhuet@gmail.com>
2023-05-08 18:25:51 +0700

    update name of window

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ Game *game = nullptr;
 int main(int argc, char *args[])
 {
     game = new Game();
-    game->init("Hello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
+    game->init("Spirit Warrior", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
     
     Uint32 frameStart;
     int frameTime;
